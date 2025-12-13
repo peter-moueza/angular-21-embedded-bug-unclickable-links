83aa8db7e24dc8af55a85d43811471478869c08a
moueza <221372+moueza@users.noreply.github.com>
2025-12-13 12:58:28 +0100

    Chore: sov

---
+++ embedded-bug-unclickable-links/src/app/a/b/c/d/d.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-d',
+  imports: [],
+  templateUrl: './d.component.html',
+  styleUrl: './d.component.css'
+})
+export class DComponent {
+
+}
