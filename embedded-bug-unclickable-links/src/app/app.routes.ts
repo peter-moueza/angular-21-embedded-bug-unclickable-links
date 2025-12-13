6fdb0b6eaaaf8ca45987f9e89c2998eeadbacdfa
moueza <221372+moueza@users.noreply.github.com>
2025-12-13 13:58:43 +0100

    Fix: A link
    import {RouterLink} from '@angular/router';
    
    Refs: Resolves #1 Unclickable links

--- embedded-bug-unclickable-links/src/app/app.routes.ts
+++ embedded-bug-unclickable-links/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './a/b/b.component';
+import { CComponent } from './a/b/c/c.component';
+import { DComponent } from './a/b/c/d/d.component';
+import { EComponent } from  './a/b/c/d/e/e.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+    path: 'a',
+    component: AComponent,
+  },
+
+ {
+    path: 'b',
+    component: BComponent,
+  },
+  ];
